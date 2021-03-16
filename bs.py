6c10796c28232d1b2a42726f0a1ae2db3fdee1da
r0ma <mymail@nxt.ru>
2021-03-16 07:56:38 +0500

    Добавлена кнопка <очистить>

--- bs.py
+++ bs.py
@@ -9,7 +9,13 @@ window.title("Поиск БС")
 window.resizable(0, 0)
 
 # window.iconbitmap('@bs.xbm')
+def clear():
+	entry.delete(0, END)
 
+def clearTextArea():
+	text.delete(1.0, END)
+	
+	
 # всплывающее окошко
 def dialog():
 	box.showinfo( 'Поиск БС', lac_entry.get() + "-" + cid_entry.get() + "\n" + "ничего не найдено")
@@ -31,7 +37,7 @@ def getInfo():
 	# если нашли то, что искали
 	if (counter[0] > 0):
 		row = cursor.execute("SELECT `EndTime`, `Address`, `Lac`, `Cellid`, `Azimut`, `BSFrequency` FROM `bs` WHERE Lac = '" + lac_entry.get() + "' AND Cellid = '" + cid_entry.get() + "' AND `EndTime` > datetime('now')").fetchone()
-		text.insert( '1.0', '----------------------------------------------------------------------------' )
+		text.insert( '1.0', '---------------------------------------------------------' )
 		text.insert( '1.0', '\n\n')
 		text.insert( '1.0', row[5] )
 		text.insert( '1.0', 'частота: ' )
@@ -61,28 +67,29 @@ lac_entry = Entry( window, width = 11 )
 cid_entry = Entry( window, width = 11 )
 
 # область для вывода ответов на запросы к БД
-text = Text(width = 45, height = 29, bg = "white", fg = 'black', wrap = WORD, font=("Verdana", 10) )
+text = Text(width = 51, height = 26, bg = "white", fg = 'black', wrap = WORD, font=("Verdana", 10) )
 text.place( x = 8, y = 65 )
 
 
 label_lac = Label( window, text = 'LAC: ' )
 label_cid = Label( window, text = 'CID: ' )
 btn = Button( window, text = 'Поиск', command = getInfo )
+btn_clear = Button( window, text = 'Очистить', command = clearTextArea)
 
 
 # расположение элементов в окне
 label_lac.place( x = 5, y = 20 )
-# operator_list.place( x = 50, y = 15 )
-label_cid.place( x = 165, y = 20 )
+label_cid.place( x = 120, y = 20 )
 
-lac_entry.place( x = 50, y = 20 )
-cid_entry.place( x = 215, y = 20 )
+lac_entry.place( x = 40, y = 20 )
+cid_entry.place( x = 150, y = 20 )
 
-btn.place( x = 340, y = 15 )
+btn.place( x = 250, y = 18 )
+btn_clear.place( x = 360, y = 18)
 
 
 # размеры окна
-window_height = 600
+window_height = 500
 window_width = 430
 
 # показываем окно по центру
